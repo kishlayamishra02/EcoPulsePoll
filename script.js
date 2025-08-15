b4691470e4100f8ffadb684f1c37b2b383d04d58
Kishlaya Mishra <144093187+kishlayamishra02@users.noreply.github.com>
2025-08-15 09:20:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
@@ -7,7 +7,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-};
+};*/
   
   firebase.initializeApp(firebaseConfig);
   const db = firebase.database();
@@ -125,4 +125,5 @@ const firebaseConfig = {
     applyBackgroundTheme(initialVote);
   }
   
+
 
