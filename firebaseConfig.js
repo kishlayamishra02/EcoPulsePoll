e947daf5fbbe5eb34b841ba92f3557928b56e585
Kishlaya Mishra <144093187+kishlayamishra02@users.noreply.github.com>
2025-08-15 09:00:29 +0530

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDbkch3tEIbcecmakhCpggDkQimfKD6XFk",
+  authDomain: "community-poll.firebaseapp.com",
+  databaseURL: "https://community-poll-default-rtdb.firebaseio.com",
+  projectId: "community-poll",
+  storageBucket: "community-poll.firebasestorage.app",
+  messagingSenderId: "535353009519",
+  appId: "1:535353009519:web:da7a102056bfdf7826349a",
+  measurementId: "G-80SR9GT34R"
+};
